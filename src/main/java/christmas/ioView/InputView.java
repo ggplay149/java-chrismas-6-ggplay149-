07001c7c52b52c8cade69c4df548a855ac3d9a96
syou149149 <syou149149@gmail.com>
2024-11-15 13:57:25 +0900

    feat($input) : 날짜 정상입력과 잘못된 입력 구분 로직 구현

---
+++ src/main/java/christmas/ioView/InputView.java
@@ -0,0 +1,13 @@
+package christmas.ioView;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public int readDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        int inputDate = Integer.parseInt(Console.readLine());
+        if(inputDate < 1 || inputDate >31) throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        return inputDate;
+    }
+}
--- src/test/java/christmas/ioVeiw/InputVeiwUnitTest.java
+++ src/test/java/christmas/ioVeiw/InputVeiwUnitTest.java
@@ -19,7 +19,7 @@ public class InputVeiwUnitTest {
         this.inputVew = new InputView();    }
 
     @Test
-    @DisplayName("정상 날짜 입력시 정상 날짜 입력")
+    @DisplayName("정상 날짜 입력시 날짜 리턴")
     public void given_input_normal_when_readDate_then_return_date() {
         //given
         String testDate = "3";
@@ -31,14 +31,28 @@ public class InputVeiwUnitTest {
     }
 
     @Test
-    @DisplayName("1~31 외 날짜 입력시 에러메세지")
-    public void given_input_wrong_date_when_readDate_then_error_message() {
+    @DisplayName("잘못된 날짜(0) 입력시 에러메세지")
+    public void given_input_wrong0_date_when_readDate_then_error_message() {
+        //given
+        String testDate = "0";
+        //when
+        System.setIn(new ByteArrayInputStream((testDate.getBytes())));
+        Exception e = assertThrows(IllegalArgumentException.class,()->inputVew.readDate());
+        //then
+        assertEquals(e.getClass(),IllegalArgumentException.class);
+        assertEquals(e.getMessage(), "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
+    @Test
+    @DisplayName("잘못된 날짜(32) 입력시 에러메세지")
+    public void given_input_wrong32_date_when_readDate_then_error_message() {
         //given
         String testDate = "32";
         //when
         System.setIn(new ByteArrayInputStream((testDate.getBytes())));
         Exception e = assertThrows(IllegalArgumentException.class,()->inputVew.readDate());
         //then
-        assertEquals(e, "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        assertEquals(e.getClass(),IllegalArgumentException.class);
+        assertEquals(e.getMessage(), "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
 }
