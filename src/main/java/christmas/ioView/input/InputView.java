d6d51bfa2dd5fc752663f7822631c2ca3b91cc16
ggplay149 <ggplay149@gmail.com>
2024-11-16 01:45:42 +0900

    feat($I/O) : I/O 구현 완성

--- src/main/java/christmas/ioView/input/InputView.java
+++ src/main/java/christmas/ioView/input/InputView.java
@@ -5,15 +5,13 @@ import christmas.menu.Menu;
 
 public class InputView {
 
-    public int readDate() {
+    public String readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        int inputDate = Integer.parseInt(Console.readLine());
-        return inputDate;
+        return Console.readLine();
     }
 
     public String readMenu() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        String inputMenu = Console.readLine();
-        return inputMenu;
+        return Console.readLine();
     }
 }
--- src/main/java/christmas/ioView/output/OutputView.java
+++ src/main/java/christmas/ioView/output/OutputView.java
@@ -1,14 +1,61 @@
 package christmas.ioView.output;
 
+import christmas.order.Order;
+import christmas.promotion.Promotion;
+import christmas.promotion.PromotionAdaptor;
+
+import java.util.List;
+
 public class OutputView {
 
-    public void printMenu() {
-        System.out.println("<주문 메뉴>");
-        System.out.println("<할인 전 총주문 금액>");
-        System.out.println("<증정 메뉴>");
-        System.out.println("<혜택 내역>");
-        System.out.println("<총혜택 금액>");
-        System.out.println("<할인 후 예상 결제 금액>");
-        System.out.println("<12월 이벤트 배지>");
+    final PromotionAdaptor promotionAdaptor;
+
+    public OutputView(PromotionAdaptor promotionAdaptor) {
+        this.promotionAdaptor = promotionAdaptor;
+    }
+
+
+    public void printResult(int inputDate, List<Order> inputOrder, int beforeTotalPrice) {
+
+        System.out.println("12월 " + inputDate + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+
+        System.out.println("\n<주문 메뉴>");
+        for (Order order : inputOrder) System.out.println(order.name() + " " + order.quantity() + "개");
+
+        System.out.println("\n<할인 전 총주문 금액>");
+        System.out.println(beforeTotalPrice + "원");
+
+        System.out.println("\n<증정 메뉴>");
+        System.out.println(promotionAdaptor.presentGift(beforeTotalPrice));
+
+        boolean promotionFlag = promotionAdaptor.isPromotionApplicable(beforeTotalPrice);
+
+        if(promotionFlag) {
+            List<Promotion> promotionList = promotionAdaptor.getAdaptedPromotionList(inputDate, beforeTotalPrice, inputOrder);
+            System.out.println("\n<혜택 내역>");
+            for (Promotion promotion : promotionList)
+                System.out.println(promotion.name() + ": -" + promotion.minusPrice() + "원");
+
+            System.out.println("\n<총혜택 금액>");
+            System.out.println("-" + promotionAdaptor.totalDiscountedPrice(promotionList) + "원");
+
+            System.out.println("\n<할인 후 예상 결제 금액>");
+            System.out.println((beforeTotalPrice-promotionAdaptor.totalPaymentPrice(promotionList))+"원");
+        }
+
+        if(!promotionFlag){
+            System.out.println("\n<혜택 내역>");
+            System.out.println("없음");
+
+            System.out.println("\n<총혜택 금액>");
+            System.out.println("0원");
+
+            System.out.println("\n<할인 후 예상 결제 금액>");
+            System.out.println(beforeTotalPrice+"원");
+
+        }
+
+        System.out.println("\n<12월 이벤트 배지>");
+        System.out.println(promotionAdaptor.presentBadge(beforeTotalPrice));
     }
 }
