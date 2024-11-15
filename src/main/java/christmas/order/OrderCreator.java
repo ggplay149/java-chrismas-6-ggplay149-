e8c7398ec7856a3f68645d74f8b0d6e62568633a
syou149149 <syou149149@gmail.com>
2024-11-15 17:34:20 +0900

    fix($Order) : Order 프로모션 적용전 주문 total 금액 구하는 기능 테스트 작성 및 구현

--- src/main/java/christmas/order/OrderCreator.java
+++ src/main/java/christmas/order/OrderCreator.java
@@ -1,25 +1,37 @@
 package christmas.order;
 
-import christmas.menu.Menu;
+import christmas.menu.MenuDataFixture;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class OrderCreator {
 
+    private MenuDataFixture menu;
+
+    public OrderCreator(MenuDataFixture menu) {
+        this.menu = menu;
+    }
+
     public List<Order> makeList(String input) {
 
         String[] inputList = input.split(",");
-        List<Order> menuList = new ArrayList<>();
+        List<Order> orderList = new ArrayList<>();
 
         for(String inputMenu : inputList){
-
             String[] NameAndQuantity = inputMenu.split("-");
             String name = NameAndQuantity[0];
             int quantity = Integer.parseInt(NameAndQuantity[1]);
-            menuList.add(new Order(name,quantity));
+            orderList.add(new Order(name,quantity));
         }
 
-        return menuList;
+        return orderList;
     }
+
+    public int countTotalPrice(List<Order> orderList) {
+        int totalPrice = 0;
+        for (Order order : orderList) totalPrice += (menu.search(order.name()).price()) * (order.quantity());
+        return totalPrice;
+    }
+
 }
--- src/test/java/christmas/order/OrderCreatorUnitTest.java
+++ src/test/java/christmas/order/OrderCreatorUnitTest.java
@@ -1,5 +1,6 @@
 package christmas.order;
 
+import christmas.menu.MenuDataFixture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,10 +12,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class OrderCreatorUnitTest {
 
     private OrderCreator orderCreator;
+    private MenuDataFixture menu;
 
     @BeforeEach
     void setUp() {
-        this.orderCreator = new OrderCreator();
+        menu = new MenuDataFixture();
+        this.orderCreator = new OrderCreator(menu);
     }
 
     @Test
@@ -28,5 +31,16 @@ public class OrderCreatorUnitTest {
         assertEquals(list.size(), 4);
         assertEquals(list.get(1).name(), "바비큐립");
         assertEquals(list.get(2).quantity(), 2);
+    }
+
+    @Test
+    @DisplayName("input 값 메뉴 리스트형식으로 만들고 총가격 리턴")
+    public void given_input_when_countTotalPrice_then_return_price() {
+        //given
+        String input = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
+        //when
+        int afterPrice = orderCreator.countTotalPrice(orderCreator.makeList(input));
+        //then
+        assertEquals(afterPrice, 142000);
     }
 }
